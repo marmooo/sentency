cf0ecf0d4213daba9a1027538869478031aed7e5
marmooo <marmooo@users.noreply.github.com>
2021-08-08 15:58:25 +0900

    bump bootstrap from 5.0.2 to 5.1.0

--- src/sw.js
+++ src/sw.js
@@ -10,7 +10,7 @@ var urlsToCache = [
   '/sentency/keyboard.mp3',
   '/sentency/favicon/original.svg',
   'https://marmooo.github.io/fonts/textar-light.woff2',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css',
 ];
 
 self.addEventListener('install', function(event) {
