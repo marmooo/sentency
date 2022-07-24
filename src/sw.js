1d03b6a85ee72d052893aa759d1dc5a9a1e71dfa
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:14 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-06-09 11:00";
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/sentency/",
   "/sentency/index.js",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/sentency/index.js",
   "/sentency/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
