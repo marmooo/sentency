35c32f50d990439439c36a32a8b99cb4aa7faacf
marmooo <marmooo@users.noreply.github.com>
2026-02-28 00:14:56 +0900

    improve dark-mode

--- src/index.js
+++ src/index.js
@@ -32,9 +32,6 @@ loadVoices();
 loadConfig();
 
 function loadConfig() {
-  if (localStorage.getItem("darkMode") === "1") {
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
   if (localStorage.getItem("bgm") === "0") {
     document.getElementById("bgmOn").classList.add("d-none");
     document.getElementById("bgmOff").classList.remove("d-none");
@@ -42,13 +39,12 @@ function loadConfig() {
 }
 
 function toggleDarkMode() {
-  if (localStorage.getItem("darkMode") === "1") {
-    localStorage.setItem("darkMode", "0");
-    document.documentElement.setAttribute("data-bs-theme", "light");
-  } else {
-    localStorage.setItem("darkMode", "1");
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
+  const html = document.documentElement;
+  const newTheme = html.getAttribute("data-bs-theme") === "dark"
+    ? "light"
+    : "dark";
+  html.setAttribute("data-bs-theme", newTheme);
+  localStorage.setItem("darkMode", newTheme);
 }
 
 function toggleBGM() {
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "2026-01-09 00:00";
+const cacheName = "2026-02-28 00:00";
 const urlsToCache = [
   "/sentency/index.js",
   "/sentency/mp3/bgm.mp3",
